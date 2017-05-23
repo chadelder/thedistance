90bd33449ccda9821486b93448bd26a2f9a4535d
chadelder <c.j.elder@outlook.com>
2017-05-22 22:21:47 -0400

    52217

--- public/route.js
+++ public/route.js
@@ -10,13 +10,25 @@ app.config(function($stateProvider, $urlRouterProvider) {
     url: '/main',
     templateUrl: 'views/main.html'
   })
-  .state('page2', {
-  url: '/page2',
-  templateUrl: 'static/page2.html'
+  .state('state1', {
+    url: '/state1',
+    templateUrl: 'views/state1.html'
   })
-  .state('page3', {
-  url: '/hello',
-  templateUrl: 'static/page3.html'
+  .state('state2', {
+  url: '/state2',
+  templateUrl: 'views/state2.html'
+  })
+  .state('state3', {
+  url: '/state3',
+  templateUrl: 'views/state3.html'
+  })
+  .state('state4', {
+  url: '/state4',
+  templateUrl: 'views/state4.html'
+  })
+  .state('state5', {
+  url: '/state5',
+  templateUrl: 'views/state5.html'
   });
 
   $urlRouterProvider.otherwise('/');
